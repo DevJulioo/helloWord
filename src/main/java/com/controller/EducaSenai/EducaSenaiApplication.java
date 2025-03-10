3bf41aab289c05d162c6e43724cb08896944a540
Júlio Cesar De Souza Moura <julioocesarmoura2008@gmail.com>
2025-03-10 16:26:19 -0300

    Add files via upload

---
+++ src/main/java/com/controller/EducaSenai/EducaSenaiApplication.java
@@ -0,0 +1,13 @@
+package com.controller.EducaSenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducaSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducaSenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controller/EducaSenai/EducaSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controller.EducaSenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducaSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
